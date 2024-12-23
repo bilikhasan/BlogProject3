f2ffdcf908ac56f79c1ce2ece3047e6381ceb1ea
bilikhasan <fatherlyy@hotmail.com.tr>
2024-12-23 23:56:56 +0300

    EntityClass oluşturuldu. Context dolduruluyor

--- BlogProject3.DataAccessLayer/Context/BlogContext.cs
+++ BlogProject3.DataAccessLayer/Context/BlogContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlogProject3.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace BlogProject3.DataAccessLayer.Context                      //Temel CRUD işlemleri gerçekleştirilir.
 {                                                                   //Entity e özgü metotlar yazılır.
-    public class BlogContext 
+    public class BlogContext :DbContext
     {
-
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<TagCloud> TagClouds { get; set; }
     }
 }
