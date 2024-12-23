aedbadc04623ee435f21ce1fe328656ab69a38b8
bilikhasan <fatherlyy@hotmail.com.tr>
2024-12-23 23:42:50 +0300

    Add project files.

---
+++ BlogProject3.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject3.DataAccessLayer.Context                      //Temel CRUD işlemleri gerçekleştirilir.
+{                                                                   //Entity e özgü metotlar yazılır.
+    public class BlogContext 
+    {
+
+    }
+}
