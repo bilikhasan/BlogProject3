7079a8cb7842248da97d960e3407b4aff2bdbf05
bilikhasan <fatherlyy@hotmail.com.tr>
2025-01-08 00:42:23 +0300

    EditMyProfile

--- BlogProject3.PresentationLayer/Areas/Author/Controllers/CommentController.cs
+++ BlogProject3.PresentationLayer/Areas/Author/Controllers/CommentController.cs
@@ -10,18 +10,50 @@ namespace BlogProject3.PresentationLayer.Areas.Author.Controllers
     {
         private readonly ICommentService _commentService;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IArticleService _articleService;
 
-        public CommentController(ICommentService commentService, UserManager<AppUser> userManager)
+        public CommentController(ICommentService commentService, UserManager<AppUser> userManager, IArticleService articleService)
         {
             _commentService = commentService;
             _userManager = userManager;
+            _articleService = articleService;
         }
 
-        public async Task <IActionResult> MyCommentList()
+        public async Task<IActionResult> MyCommentList()
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             var values = _commentService.TGetCommentsByAppUserId(user.Id);
             return View(values);
+        }
+
+        public async Task<IActionResult> CommentDelete(int id)
+        {
+            _commentService.TDelete(id);
+            return RedirectToAction("MyCommentList");
+        }
+
+
+        [HttpGet]
+        public IActionResult CommentUpdate(int id)
+        {
+            var value = _commentService.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CommentUpdate(Comment comment)
+        {
+            ModelState.Clear();
+
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            comment.AppUserId = values.Id;
+            comment.CreatedDate = DateTime.Now;
+            comment.Status = true;
+            comment.ArticleId = values.Id;
+            comment.CommentId = values.Id;
+
+            _commentService.TUpdate(comment);
+            return RedirectToAction("MyCommentList");
         }
     }
 }
